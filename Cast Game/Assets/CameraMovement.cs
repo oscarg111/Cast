19cef03f6ffad379f61e954f9f9296c6b0931491
zm68526 <104472530+zm68526@users.noreply.github.com>
2023-03-23 21:37:37 -0400

    Rudimentary WaterMage implementation
    I created a rudimentary version of the Water Mage by copy-pasting the Fire Mage object. For now, it functions identically to the fire mage. I also updated CameraMovement.cs so that the camera is placed between them. For now, everything else (HordeSpawner.cs, ChaseAi.cs) still uses the fire mage's position. Gonna hold off on adding the other 2 in case I unintentionally introduced a problem or in case there's a more optimal way to do it.

--- Cast Game/Assets/CameraMovement.cs
+++ Cast Game/Assets/CameraMovement.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public GameObject player;
+    public GameObject fireMage;
+    public GameObject waterMage;
 
     // Start is called before the first frame update
     void Start()
@@ -15,8 +16,28 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (player == null) this.enabled = false;
-        else transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+        // if (player == null) this.enabled = false;
+        transform.position = getAveragePlayerPosition();
         
+    }
+
+    Vector3 getAveragePlayerPosition() {
+        float x = 0f;
+        float y = 0f;
+        int n = 0; // num of alive mages
+        if (fireMage != null) {
+            x += fireMage.transform.position.x;
+            y += fireMage.transform.position.y;
+            n++;
+        }
+
+        if (waterMage != null) {
+            x += waterMage.transform.position.x;
+            y += waterMage.transform.position.y;
+            n++;
+        }
+
+        if (n == 0) return transform.position;
+        else return new Vector3(x/n, y/n, transform.position.z);
     }
 }
