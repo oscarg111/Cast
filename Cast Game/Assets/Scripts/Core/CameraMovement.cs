8a17a1e69a32fbcaff07c7cc2437ad5649b70a38
BoatK <jmartinobst@gmail.com>
2023-04-18 21:08:48 -0400

    Added Dynamic Camera, Changed Enemy Hitbox

--- Cast Game/Assets/Scripts/Core/CameraMovement.cs
+++ Cast Game/Assets/Scripts/Core/CameraMovement.cs
@@ -6,18 +6,78 @@ public class CameraMovement : MonoBehaviour
 {
     public GameObject fireMage;
     public GameObject waterMage;
+    public float minHeight;
+    public float maxHeight;
+    public float paddingPercent;
+    public float cameraAccelRate;
+    private float aspect;
+    private float height;
+    private float width;
+    private float goalWidthSize;
+    private float goalHeightSize;
+    private float goalHeight;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        aspect = (float)Screen.width / Screen.height;
+
+        height = GetComponent<Camera>().orthographicSize * 2;
+
+        width = height * aspect;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        height = GetComponent<Camera>().orthographicSize * 2;
+        width = height * aspect;
         // if (player == null) this.enabled = false;
         transform.position = getAveragePlayerPosition();
+        if(waterMage != null && fireMage != null)
+        {
+            goalWidthSize = Mathf.Abs(waterMage.transform.position.x - fireMage.transform.position.x) * (1 + paddingPercent);
+            goalWidthSize /= aspect;
+            goalHeightSize = Mathf.Abs(waterMage.transform.position.y - fireMage.transform.position.y) * (1 + paddingPercent);
+            if (goalWidthSize > goalHeightSize)
+            {
+                if(goalWidthSize/2 < minHeight)
+                {
+                    goalHeight = minHeight;
+                }
+                else if(goalWidthSize / 2 > maxHeight)
+                {
+                    goalHeight = maxHeight;
+                }
+                else
+                {
+                    goalHeight = goalWidthSize / 2;
+                }
+            }
+            else
+            {
+                if (goalHeightSize / 2 < minHeight)
+                {
+                    goalHeight = minHeight;
+                }
+                else if (goalHeightSize / 2 > maxHeight)
+                {
+                    goalHeight = maxHeight;
+                }
+                else
+                {
+                    goalHeight = goalHeightSize / 2;
+                }
+            }
+            GetComponent<Camera>().orthographicSize = height / 2 + (goalHeight - height / 2) * Time.deltaTime * cameraAccelRate;
+        }
+        else
+        {
+            goalHeight = minHeight;
+            GetComponent<Camera>().orthographicSize = height / 2 + (goalHeight - height / 2) * Time.deltaTime * cameraAccelRate;
+        }
         
     }
 
@@ -40,4 +100,5 @@ public class CameraMovement : MonoBehaviour
         if (n == 0) return transform.position;
         else return new Vector3(x/n, y/n, transform.position.z);
     }
+
 }
