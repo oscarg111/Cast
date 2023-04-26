ba829761458ece461243ae4b60ca552d6cf87ea3
mannmannrock <124934347+mannmannrock@users.noreply.github.com>
2023-04-26 04:42:08 -0400

    Fixed TileMap Layers, Started dialogue indicator
    removed gravity, fixed walls.

---
+++ Cast Game/Assets/Scripts/Dialogue System/DialogueIndie.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueIndie : MonoBehaviour
+{
+    public GameObject alert;
+    // Update is called once per frame
+    void Update()
+    {
+        {
+            alert.SetActive(true);
+        }
+    }
+}
