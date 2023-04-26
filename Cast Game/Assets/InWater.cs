24e4314c3984f61082e2c259d94e640bcf56e987
mannmannrock <mannsytha@gmail.com>
2023-04-26 12:53:24 -0400

    Added Input Tutorial Text, Started Interactable Indicator/Prompt, Started Interactable Colliders

--- Cast Game/Assets/InWater.cs
+++ Cast Game/Assets/InWater.cs
@@ -1,32 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class DialogueIndicator : MonoBehaviour
+public class InWater : MonoBehaviour
 {
+    //public movementWaterMage 
 
-    
-    
-    public Collider2D interactable;
-    public Collider2D playerCollider;
-    
-
-    public GameObject alert;
-    // Update is called once per frame
+    // Start is called before the first frame update
     void OnTriggerEnter2D(Collider2D collision)
     {
 
         Debug.Log("Non-trigger Colliders entering Collision");
         {
-            alert.SetActive(true);
+            //movementWaterMage.withinWell(true);
         }
     }
     void OnTriggerExit2D(Collider2D collision)
     {
         Debug.Log("Non-trigger Colliders exiting Collision");
         {
-        alert.SetActive(false);
+        //movementWaterMage.withinWell(true);
         }
     }
 }
